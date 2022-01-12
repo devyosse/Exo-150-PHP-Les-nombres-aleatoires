df9aaef677e249a8ab53a8d2f2e2461f3bdde12c
devyosse <narutoshippuden884@gmail.com>
2022-01-12 17:21:25 +0100

    exo 150

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  */
 
 // TODO Votre code ici.
-
-
+$random = rand(0, 50);
+echo "<div>$random</div>";
 
 /**
  * 2. Générez un nombre aléatoire compris entre 50 et getrandmax, affichez le résultat dans un nouveau <p>
@@ -14,7 +14,9 @@
 
 // TODO Votre code ici.
 
-
+echo "<p>Valeur :" . getrandmax() . "</p>";
+$rando = rand(0, 50);
+echo $rando;
 /**
  * 3. Générez un nombre aléatoire compris entre 0 et 50
  * ==> si le nombre est inférieur ou égal à 25, affichez : vous avez gagné ( dans un nouveau <p> )
@@ -22,7 +24,13 @@
  */
 
 // TODO Votre code ici.
-
+$ran = rand(0, 50);
+if ( $ran <= 25 ){
+    echo "<p>Vous avez gagné</p>";
+}
+else{
+    echo "<p>Vous avez perdu</p>";
+}
 
 /**
  * 4. Créez une fonction qui génère un nombre aléatoire compris entre $max et $min ( paramètres ).
@@ -32,9 +40,16 @@
 
 // TODO Votre code ici.
 
+function maFunction(rand $max){
+    if ($max === -100){
+        echo $max;
+    }
+    else{
+        return $max;
+    }
+}
 
-
-
+maFunction(-100);
 
 
 
